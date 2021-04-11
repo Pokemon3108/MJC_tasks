aa2234c632a0e7eebe44d674f4b08e681ed76c90
Darya Zalevskaya <50102121+Pokemon3108@users.noreply.github.com>
2021-04-11 23:31:01 +0300

    removed Task1 from this branch

--- Task1/multi-project/api/src/main/java/by/daryazalevskaya/apimodule/App.java
+++
@@ -1,9 +0,0 @@
-package by.daryazalevskaya.apimodule;
-
-import by.daryazalevskaya.coremodule.Utils;
-
-public class App {
-    public static void main(String[] args) {
-        System.out.println(Utils.isAllPositiveNumbers("12", "79"));
-    }
-}
